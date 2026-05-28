185b13a300a59e6b2c863b79abc4bb07a9fdaef1
Kyle Li <8055969@qq.com>
2026-05-28 18:29:38 +0800

    Fix Render static deploy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
             manifest: false,
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
                 runtimeCaching: [
                     {
                         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
